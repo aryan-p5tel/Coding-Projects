fd46700bc885f177a1d88fe4003a328077e014d7
Aryan <fst.openacc@gmail.com>
2025-08-18 00:45:07 +0100

    Add files via upload

---
+++ Tic Tac Toe/main.py
@@ -0,0 +1,13 @@
+
+import tkinter
+
+#Setting up the Window
+
+window = tkinter.Tk()
+window.title("Graphical User Interface")
+window.geometry("400x400")
+window.configure(background = "lightblue")
+
+
+#very important and starts GUI loop
+window.mainloop()
