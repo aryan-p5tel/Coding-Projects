d37088196d1ea04ca377070f7f5e852237256885
Aryan <fst.openacc@gmail.com>
2025-08-18 02:01:42 +0100

    Further tests
    Including labels and further understanding how GUI works + python code behind it

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -8,6 +8,11 @@ window.title("Graphical User Interface")
 window.geometry("400x400")
 window.configure(background = "lightblue")
 
+#adding elements to the window
+label1=tkinter.Label(window, text="Hello there!",fg = "black", bg = "lightblue", font = ("Arial", 12))
+
+#this will allow the label to be displayed in the window
+label1.pack(pady=20)  # Adds some vertical padding
 
 #very important and starts GUI loop
 window.mainloop()
