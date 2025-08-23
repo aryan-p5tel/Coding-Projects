0fd0315143fea2942e5d02d57b5ecfa5351c1e74
Aryan <fst.openacc@gmail.com>
2025-08-23 22:10:05 +0100

    basic text update
    includes next step (to be learnt - how to code columns and rows and how they work

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -4,12 +4,12 @@ import tkinter
 #Setting up the Window
 
 window = tkinter.Tk()
-window.title("Graphical User Interface")
+window.title("Tic Tac Toe Game")
 window.geometry("400x400")
 window.configure(background = "lightblue")
 
 #adding elements to the window
-label1=tkinter.Label(window, text="Hello there!",fg = "black", bg = "lightblue", font = ("Arial", 12))
+label1=tkinter.Label(window, text="Tic Tac Toe",fg = "black", bg = "lightblue", font = ("Arial", 12))
 
 #this will allow the label to be displayed in the window
 label1.pack(pady=20)  # Adds some vertical padding
@@ -19,3 +19,4 @@ window.mainloop()
 
 ##in progress
 
+#Uses columns and rows to position elements
