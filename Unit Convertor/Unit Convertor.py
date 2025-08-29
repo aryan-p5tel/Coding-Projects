e87732aafe36694e0c823366f8394f66d4ddd7ad
Aryan <fst.openacc@gmail.com>
2025-08-30 00:24:36 +0100

    intro
    intro

---
+++ Unit Convertor/Unit Convertor.py
@@ -0,0 +1,5 @@
+#Unit convertor
+
+print("Welcome to Unit Convertor :\n 1. Length \n2. Weight \n3. Temperature\n4. Speed\n\n Select the type of conversion you want to perform (1-4): ")
+choice=int(input("Enter : "))
+if choice==1: #continue later
