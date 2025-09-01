43d751200c9957df33ba06c83bfed67456310518
Aryan <fst.openacc@gmail.com>
2025-09-01 01:22:17 +0100

    length+options

--- Unit Convertor/Unit Convertor.py
+++ Unit Convertor/Unit Convertor.py
@@ -2,4 +2,33 @@
 
 print("Welcome to Unit Convertor :\n 1. Length \n2. Weight \n3. Temperature\n4. Speed\n\n Select the type of conversion you want to perform (1-4): ")
 choice=int(input("Enter : "))
-if choice==1: #continue later
+if choice==1:
+    print("You have selected Length conversion")
+    print("1. Meter to Kilometer \n2. Kilometer to Meter \n3. Meter to Centimeter \n4. Centimeter to Meter \n5. Kilometer to Centimeter \n6. Centimeter to Kilometer")
+    length_choice=int(input("Enter your choice (1-6): "))
+    if length_choice==1:
+        meter=float(input("Enter length in metres: "))
+        kilometer=meter/1000
+        print(f"{meter} metres is equal to {kilometer} kilometers")
+    elif length_choice==2:
+        kilometer=float(input("Enter length in kilometers: "))
+        meter=kilometer*1000
+        print(f"{kilometer} kilometers is equal to {meter} metres")
+    elif length_choice==3:
+        meter=float(input("Enter length in metres: "))
+        centimeter=meter*100
+        print(f"{meter} metres is equal to {centimeter} centimeters")
+    elif length_choice==4:
+        centimeter=float(input("Enter length in centimeters: "))
+        meter=centimeter/100
+        print(f"{centimeter} centimeters is equal to {meter} metres")
+    elif length_choice==5:
+        kilometer=float(input("Enter length in kilometers: "))
+        centimeter=kilometer*100000
+        print(f"{kilometer} kilometers is equal to {centimeter} centimeters")
+    elif length_choice==6:
+        centimeter=float(input("Enter length in centimeters: "))
+        kilometer=centimeter/100000
+        print(f"{centimeter} centimeters is equal to {kilometer} kilometers")
+    else:
+        print("Invalid choice")
